8726f38794ba1444b10198739bbd8f95c7c9f431
Mausumi Patra <someepatra@gmail.com>
2019-12-18 14:10:09 -0500

    updated backend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://alphabets-game.surge.sh'
+    origins ['https://alphabets-game.surge.sh','http://localhost:3001']
 
     resource '*',
       headers: :any,
